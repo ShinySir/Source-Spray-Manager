626edcf6664ad54f809701f9188f9b768a702c43
puro gaming <shinymegalucariods@gmail.com>
2026-03-11 20:59:40 +0800

    thing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use eframe::*;
+use egui::CentralPanel;
+
+struct MyApp {}
+
+impl eframe::App for MyApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        CentralPanel::default().show(ctx, |ui| {
+            ui.label("Hello, world!");
+        });
+    }
+}
+
+fn main() -> eframe::Result<(), eframe::Error> {
+    run_native("Source Spray Manager",
+    NativeOptions::default(),
+    AppCreator::Box::new(| cc: &CreationContext<'_>|{
+        Box::new(MyApp {})
+    }));
 }
 //test
