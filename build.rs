3336661a1060835fab42bc923c259f842de1dbe4
puro gaming <shinymegalucariods@gmail.com>
2026-03-14 22:51:59 +0800

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,12 +1,11 @@
-#[cfg(windows)]
-use winres;
-
+use std::env;
 
 fn main() {
-	#[cfg(windows)]
-	{
-    let mut res = winres::WindowsResource::new();
-    res.set_icon("assets/icon.ico");
-    res.compile().expect("Failed to compile Windows resources");
-	}
+    if env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
+        println!("cargo:rerun-if-changed=assets/icon.ico");
+
+        let mut res = winres::WindowsResource::new();
+        res.set_icon("assets/icon.ico");
+        res.compile().expect("Failed to compile Windows resources");
+    }
 }
